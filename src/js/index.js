4008a826fc6525c1a812a97623c4aa25bb1bdfce
derasan <fibonacci>
2018-08-02 22:35:17 +0400

    refactoring

--- src/js/index.js
+++ src/js/index.js
@@ -21,24 +21,23 @@ let message = document.getElementById('message');
 let state = false;
 let resultsCursor = 0;
 
-function toggleResults(action) {
+let toggleResults = (action) => {
     results.classList.toggle(CSS_VISIBLE, action === SHOW);
-}
+};
 
-function getMatches(value) {
+let getMatches = (value) => {
     let matchList = [];
-    let dataLength = cities.length;
-
-    for (let i = 0; i < dataLength; i++) {
-        if (cities[i].City.toLowerCase().indexOf(value.toLowerCase()) != -1) {
-            matchList.push(cities[i].City);
+    
+    for (let city of cities) {
+        if (city.City.toLowerCase().indexOf(value.toLowerCase()) != -1) {
+            matchList.push(city.City);
         }
     }
 
     return matchList;
-}
+};
 
-function displayMatches(matchList) {
+let displayMatches = (matchList) => {
     let matchListLength = matchList.length;
 
     if (matchListLength > MATCHES_COUNT) {
@@ -50,63 +49,62 @@ function displayMatches(matchList) {
     }
 
     toggleResults(SHOW);
-}
+};
 
-function createResultList(length, list) {
+let createResultList = (length, list) => {
     let j = 0;
     
     while (j < length) {
         results.innerHTML += '<li class = "results">' + list[j] + '</li>';
         j++;
     }
-}
+};
 
-function moveCursor(pos) {
-    let resLength = results.children.length;
+let  moveCursor = (pos) => {
+    let resChildren = results.children;
 
-    for (let x = 0; x < resLength; x++) {
-        results.children[x].classList.remove(CSS_HIGHLIGHTED);
+    for (let result of resChildren) {
+        result.classList.remove(CSS_HIGHLIGHTED);
     }
 
     results.children[pos].classList.add(CSS_HIGHLIGHTED);
-}
+};
 
-function addValueMassage() {
-    if ((state === false)&&(targetInput.value)) {
-        showMessage('Добавьте значение в справочник или выберите другое значение из списка');
-        toggleError(SHOW);
+let addValueMassage = () => {
+    if (!(state === false)||!(targetInput.value)) return;
+    
+    showMessage('Добавьте значение в справочник или выберите другое значение из списка');
+    toggleError(SHOW);
 
-        toggleResults(HIDE);
-        results.innerHTML = "";
-    }
-}
+    toggleResults(HIDE);
+    results.innerHTML = "";
+};
 
-function showMessage(text) {
+let showMessage = (text) => {
     message.innerHTML = `<li>${text}</li>`;
-}
+};
 
-function hideMessage() {
+let hideMessage = () => {
     message.innerHTML = '';
-}
+};
 
-function toggleError(action) {
+let toggleError = (action) => {
     targetInput.classList.toggle(CSS_ERROR, action === SHOW);
     message.classList.toggle(CSS_ERROR_MESSAGE, action === SHOW);
-}
+};
 
-function mouseChoose() {
+let mouseChoose = () => {
     let moveMouse = document.querySelectorAll('.results');
-    let moveMouseLength = moveMouse.length;
-
-    for (let k = 0; k < moveMouseLength; k++) {
-        moveMouse[k].onmouseover = function () {
-            resultsCursor = k;
+    
+    for (let i in moveMouse) {
+        moveMouse[i].onmouseover = function () {
+            resultsCursor = i;
 
             moveCursor(resultsCursor);
         };
 
-        moveMouse[k].onmousedown = function () {
-            targetInput.value = results.children[k].innerHTML;
+        moveMouse[i].onmousedown = function () {
+            targetInput.value = results.children[i].innerHTML;
 
             resultsCursor = 0;
             state = true;
@@ -116,9 +114,9 @@ function mouseChoose() {
             results.innerHTML = "";
         };
     }
-}
+};
 
-function searchMatches(obj) {
+let searchMatches = (obj) => {
     if (!obj.value.length > 0) {
         return false;
     }
@@ -131,7 +129,7 @@ function searchMatches(obj) {
     } else {
         showMessage('Ничего не найдено!');
     }
-}
+};
 
 targetInput.focus();
 
